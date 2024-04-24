4e0ceff9c84580a43eece4b94e6239e0f9e02b9e
Alishba Majeed <159819963+alishbamajeed@users.noreply.github.com>
2024-04-24 19:00:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+const students = [];
+const mainMenu = () => {
+    inquirer.prompt({
+        type: 'list',
+        name: 'option',
+        message: 'Choose an option:',
+        choices: [
+            { name: 'Add Student', value: 'add' },
+            { name: 'List Students', value: 'list' },
+            { name: 'Exit', value: 'exit' }
+        ]
+    }).then((answer) => {
+        switch (answer.option) {
+            case 'add':
+                addStudent();
+                break;
+            case 'list':
+                listStudents();
+                break;
+            case 'exit':
+                console.log(chalk.yellow('Goodbye!'));
+                break;
+            default:
+                console.log(chalk.red('Invalid option'));
+        }
+    });
+};
+const addStudent = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Dear Enter student name:'
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: 'Dear Enter student ID:'
+        },
+        {
+            type: 'number',
+            name: 'grade',
+            message: 'Dear Enter student grade:'
+        }
+    ]).then((answers) => {
+        students.push(answers);
+        console.log(chalk.green('Student added successfully!'));
+        mainMenu();
+    });
+};
+const listStudents = () => {
+    console.log(chalk.blue('List of Students:'));
+    students.forEach(student => {
+        console.log(chalk.cyan(`Name: ${student.name}, ID: ${student.id}, Grade: ${student.grade}`));
+    });
+    mainMenu();
+};
+mainMenu();
